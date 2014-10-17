3000c89a83b8e37753c77f9bedbe3206eb9aff12
kimberlymartin <kimberlymartin99@gmail.com>
2014-10-17 15:00:39 -0500

    70pt

--- lab9-70pt.py
+++ lab9-70pt.py
@@ -1,8 +1,4 @@
-############################################
-#                                          # 
-#                   70pt                   #
-#                                          #
-############################################
+#70pt
 
 # Create a celcius to fahrenheit calculator.
 # Multiply by 9, then divide by 5, then add 32 to calculate your answer.
@@ -12,3 +8,8 @@
 # Accept user input 
 # Calculate fahrenheit
 # Output answer
+
+print "What's the temperature in Celcius?"
+celcius = int(raw_input())
+fahrenheit = ((celcius * 9) / 5) + 32
+print "The temperature is " + str(fahrenheit) + " degrees fahrenheit."
